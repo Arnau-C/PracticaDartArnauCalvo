5935474eda174aa0be3fb56e465489fe142f0b14
Arnau <150950867+Arnau-C@users.noreply.github.com>
2026-02-01 19:24:47 +0100

    Feat/build views (#3)
    * feat: Start menus of the app (not finished but runs)
    
    * feat: Implements CLI without logic
    
    * chore: Remove AutoTest
    
    * style: Menus finished

---
+++ lib/views/view_user.dart
@@ -0,0 +1,17 @@
+class ViewUser {
+  void printMenuUser() {
+    print("=== Menu Usuari ===");
+    print("[J] Jugar");
+    print(
+      "[B] Llistar els jocs de la botiga",
+    );
+    print("[C] Comprar joc");
+    print("[L] Llogar joc");
+    print("[P] Provar joc");
+    print("[D] Donar joc");
+    print("[A] Llistar amics");
+    print("[F] Fer nou amic");
+    print("[E] Enrere");
+    print("[T] Tancar aplicació");
+  }
+}
