d6ec07f769f2a78a388fbc1af40af5a760edfb09
Arnau <arnau.calvo6@gmail.com>
2026-02-01 18:51:18 +0100

    feat: Implements CLI without logic

---
+++ lib/views/view_game.dart
@@ -0,0 +1,10 @@
+class ViewGame {
+  void printGameMenu() {
+    print("=== Menú del Joc ===");
+    print("[H] Highscores");
+    print("[G] Grup");
+    print("[P] Puntuació");
+    print("[E] Enrere");
+    print("[T] Tancar aplicació");
+  }
+}
