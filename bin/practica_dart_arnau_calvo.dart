ed2a4b21a61ec8d777831bcb58c048dbd7755702
Arnau <arnau.calvo6@gmail.com>
2026-02-01 13:52:43 +0100

    Creation of project

---
+++ bin/practica_dart_arnau_calvo.dart
@@ -0,0 +1,5 @@
+import 'package:practica_dart_arnau_calvo/practica_dart_arnau_calvo.dart' as practica_dart_arnau_calvo;
+
+void main(List<String> arguments) {
+  print('Hello world: ${practica_dart_arnau_calvo.calculate()}!');
+}
---
+++ test/practica_dart_arnau_calvo_test.dart
@@ -0,0 +1,8 @@
+import 'package:practica_dart_arnau_calvo/practica_dart_arnau_calvo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
