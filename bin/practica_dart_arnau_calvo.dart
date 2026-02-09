ca949bdb9fd5c94fa934dbeb48909d54df6051f6
Arnau <arnau.calvo6@gmail.com>
2026-02-09 19:11:44 +0100

    style: Formatting

--- bin/practica_dart_arnau_calvo.dart
+++ bin/practica_dart_arnau_calvo.dart
@@ -1,6 +1,7 @@
 import 'package:practica_dart_arnau_calvo/view_models/app_view_model.dart';
 
 void main() {
+  // Iniciamos la aplicación creando una instancia de AppViewModel y llamando a startApp.
   final app = AppViewModel();
   app.startApp();
 }
